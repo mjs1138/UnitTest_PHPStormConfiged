b517392f68bd6495c9cdc7efd2cae33c7e0b1be7
mike-sullivan-egov <mike.sullivan@egov.com>
2018-01-24 15:27:41 -0500

    Added Tests

---
+++ tests/unit/BTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 1/18/18
+ * Time: 11:17 AM
+ */
+
+use \PHPUnit\Framework\TestCase;
+
+
+class BTest extends TestCase {
+    public function testTrueAssertsToTrue(){
+        $a=1;
+        $this->assertTrue(true);
+    }
+
+    public function testTrueAssertsToFalse(){
+        $a=1;
+        $this->assertTrue(false);
+    }
+}
